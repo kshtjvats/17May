46eed912ee577f1a04c02450a25f2afee4124298
kshtjvats <Kshitij.vats@avisoft.io>
2024-05-17 16:35:10 +0530

    First commit

---
+++ src/test/java/CRUDIOC/IOC/DEMO/IocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package CRUDIOC.IOC.DEMO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
